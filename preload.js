d9327997846dcb4af8ad6c3a9c124f755441027e
Princealex254 <senerwaalex@gmail.com>
2026-09-05 23:48:55 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electronAPI', {
-  printReceipt: (data) => ipcRenderer.send('print-receipt', data),
-  openCashDrawer: () => ipcRenderer.send('open-drawer')
-});
